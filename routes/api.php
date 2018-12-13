7e54b5725e845dd1555f0e03a7a9fbbb26c778e9
David Bastidas <espabastidas19@gmail.com>
2018-12-13 17:34:28 -0500

    fix api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+], function () {
 });
+Route::post('login', 'ApiController@login');
+Route::post('avisos/getVisitas', 'ApiController@getAvisos');
+Route::post('avisos/actualizarVisita', 'ApiController@actualizarAviso');
