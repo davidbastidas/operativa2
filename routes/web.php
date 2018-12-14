31b9336af92c56774d4d1cd285a8ecca10def958
otherside10 <jsarmientop.inca@gmail.com>
2018-12-14 09:59:16 -0500

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -21,122 +21,127 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function () {
 
-Route::get('admin/agenda', [
-    'as' => 'agenda',
-    'uses' => 'AvisosController@index'
-]);
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('admin/mapas', [
-    'as' => 'mapas',
-    'uses' => 'AvisosController@visitaMapa'
-]);
+    Route::get('admin/agenda', [
+        'as' => 'agenda',
+        'uses' => 'AvisosController@index'
+    ]);
 
-Route::match(['get', 'post'], '/admin',
-    [
-        'as' => 'admin',
-        'uses' => 'AdminController@index'
-    ]
-);
+    Route::get('admin/mapas', [
+        'as' => 'mapas',
+        'uses' => 'AvisosController@visitaMapa'
+    ]);
 
-Route::get('admin/dashboard/{id}', [
-    'as' => 'admin.dashboard',
-    'uses' => 'AdminController@dashboard'
-]);
+    Route::match(['get', 'post'], '/admin',
+        [
+            'as' => 'admin',
+            'uses' => 'AdminController@index'
+        ]
+    );
 
-Route::match(['get', 'post'], 'admin/img-panel',
-    [
-        'as' => 'admin.img',
-        'uses' => 'ImagesController@index'
-    ]
-);
+    Route::get('admin/dashboard/{id}', [
+        'as' => 'admin.dashboard',
+        'uses' => 'AdminController@dashboard'
+    ]);
 
-Route::get('admin/subir-avisos/{agenda}', [
-    'as' => 'admin.avisos.subir',
-    'uses' => 'UploadExcelController@index'
-]);
+    Route::match(['get', 'post'], 'admin/img-panel',
+        [
+            'as' => 'admin.img',
+            'uses' => 'ImagesController@index'
+        ]
+    );
 
-Route::post('admin/excel/upload', [
-    'as' => 'admin.excel.upload',
-    'uses' => 'UploadExcelController@upload'
-]);
+    Route::get('admin/subir-avisos/{agenda}', [
+        'as' => 'admin.avisos.subir',
+        'uses' => 'UploadExcelController@index'
+    ]);
 
-Route::post('admin/agenda/download', [
-    'as' => 'admin.agenda.download',
-    'uses' => 'DownloadController@download'
-]);
+    Route::post('admin/excel/upload', [
+        'as' => 'admin.excel.upload',
+        'uses' => 'UploadExcelController@upload'
+    ]);
 
-Route::post('admin/avisos/upload', [
-    'as' => 'admin.avisos.upload',
-    'uses' => 'AvisosController@subirAvisos'
-]);
+    Route::post('admin/agenda/download', [
+        'as' => 'admin.agenda.download',
+        'uses' => 'DownloadController@download'
+    ]);
 
-Route::get('admin/asignar-avisos/index/{agenda}', 'AvisosController@listaAvisosIndex')->name('asignar.avisos');
+    Route::post('admin/avisos/upload', [
+        'as' => 'admin.avisos.upload',
+        'uses' => 'AvisosController@subirAvisos'
+    ]);
 
-Route::post('admin/asignar-avisos', [
-    'as' => 'admin.asignar.avisos',
-    'uses' => 'AvisosController@cargarAvisos'
-]);
+    Route::get('admin/asignar-avisos/index/{agenda}', 'AvisosController@listaAvisosIndex')->name('asignar.avisos');
 
-Route::post('admin/asignarall', [
-    'as' => 'admin.asignarall',
-    'uses' => 'AvisosController@asignarAllAvisos'
-]);
+    Route::post('admin/asignar-avisos', [
+        'as' => 'admin.asignar.avisos',
+        'uses' => 'AvisosController@cargarAvisos'
+    ]);
 
-Route::post('admin/vaciar-carga', [
-    'as' => 'admin.vaciar.carga',
-    'uses' => 'AvisosController@vaciarCarga'
-]);
+    Route::post('admin/asignarall', [
+        'as' => 'admin.asignarall',
+        'uses' => 'AvisosController@asignarAllAvisos'
+    ]);
 
-Route::post('admin/agenda/save', [
-    'as' => 'agenda.save',
-    'uses' => 'AvisosController@saveAgenda'
-]);
+    Route::post('admin/vaciar-carga', [
+        'as' => 'admin.vaciar.carga',
+        'uses' => 'AvisosController@vaciarCarga'
+    ]);
 
-Route::get('admin/agenda/delete/{agenda}', [
-    'as' => 'agenda.delete',
-    'uses' => 'AvisosController@deleteAgenda'
-]);
+    Route::post('admin/agenda/save', [
+        'as' => 'agenda.save',
+        'uses' => 'AvisosController@saveAgenda'
+    ]);
 
-Route::get('admin/avisos/editar/{aviso}', [
-    'as' => 'aviso.editar',
-    'uses' => 'AvisosController@editarAviso'
-]);
+    Route::get('admin/agenda/delete/{agenda}', [
+        'as' => 'agenda.delete',
+        'uses' => 'AvisosController@deleteAgenda'
+    ]);
 
-Route::post('admin/avisos/save', [
-    'as' => 'aviso.editar.save',
-    'uses' => 'AvisosController@saveAviso'
-]);
+    Route::get('admin/avisos/editar/{aviso}', [
+        'as' => 'aviso.editar',
+        'uses' => 'AvisosController@editarAviso'
+    ]);
 
-Route::get('admin/getAvisos', 'AvisosController@getAvisos');
+    Route::post('admin/avisos/save', [
+        'as' => 'aviso.editar.save',
+        'uses' => 'AvisosController@saveAviso'
+    ]);
 
-Route::post('admin/dashboard/getAvancePorGestor', [
-    'as' => 'admin.dashboard.getAvancePorGestor',
-    'uses' => 'DashboardController@getAvancePorGestor'
-]);
+    Route::get('admin/getAvisos', 'AvisosController@getAvisos');
 
-Route::post('admin/dashboard/getAvanceDiario', [
-    'as' => 'admin.dashboard.getAvanceDiario',
-    'uses' => 'DashboardController@getAvanceDiario'
-]);
+    Route::post('admin/dashboard/getAvancePorGestor', [
+        'as' => 'admin.dashboard.getAvancePorGestor',
+        'uses' => 'DashboardController@getAvancePorGestor'
+    ]);
 
-Route::post('admin/dashboard/getPointMapGestores', [
-    'as' => 'admin.dashboard.getPointMapGestores',
-    'uses' => 'DashboardController@getPointMapGestores'
-]);
+    Route::post('admin/dashboard/getAvanceDiario', [
+        'as' => 'admin.dashboard.getAvanceDiario',
+        'uses' => 'DashboardController@getAvanceDiario'
+    ]);
 
-Route::get('admin/avisos/delete/{aviso}', [
-    'as' => 'aviso.eliminar',
-    'uses' => 'AvisosController@deleteAviso'
-]);
+    Route::post('admin/dashboard/getPointMapGestores', [
+        'as' => 'admin.dashboard.getPointMapGestores',
+        'uses' => 'DashboardController@getPointMapGestores'
+    ]);
 
-Route::post('admin/avisos/delete/all', [
-    'as' => 'aviso.eliminar.all',
-    'uses' => 'AvisosController@deleteAvisoPorSeleccion'
-]);
+    Route::get('admin/avisos/delete/{aviso}', [
+        'as' => 'aviso.eliminar',
+        'uses' => 'AvisosController@deleteAviso'
+    ]);
 
-Route::post('admin/avisos/getPointMapVisita', [
-    'as' => 'admin.avisos.getPointMapVisita',
-    'uses' => 'AvisosController@getPointMapVisita'
-]);
+    Route::post('admin/avisos/delete/all', [
+        'as' => 'aviso.eliminar.all',
+        'uses' => 'AvisosController@deleteAvisoPorSeleccion'
+    ]);
+
+    Route::post('admin/avisos/getPointMapVisita', [
+        'as' => 'admin.avisos.getPointMapVisita',
+        'uses' => 'AvisosController@getPointMapVisita'
+    ]);
+});
+
+
